0d2fc252af8007a4dcd97aa4579dd7329b1d71ce
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2023-01-24 12:31:53 +0530

    Model Developed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,12 +34,19 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.splitter2 = new System.Windows.Forms.Splitter();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.listBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
@@ -58,6 +65,7 @@
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.listBox2);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(203, 0);
             this.panel2.Name = "panel2";
@@ -77,11 +85,45 @@
             // 
             this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.listBox3);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(203, 158);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(488, 240);
             this.panel3.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(198, 396);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(0, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(486, 153);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(0, 0);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(486, 238);
+            this.listBox3.TabIndex = 0;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
             // 
             // WelcomeForm
             // 
@@ -97,6 +139,9 @@
             this.Name = "WelcomeForm";
             this.Text = "College Application";
             this.Load += new System.EventHandler(this.WelcomeForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -108,5 +153,8 @@
         private Panel panel2;
         private Splitter splitter2;
         private Panel panel3;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,166 @@ namespace College_Application
 {
     public partial class WelcomeForm : Form
     {
+        // Student Model
+        private string studentName = string.Empty;
+        private int studentRoll = 0;
+        private int studentMarks = 0;
+
+        // Book Model
+        private int bookId = 0 ;
+        private string tittle = string.Empty;
+        private string author = string.Empty;
+        private double price = 0.0;
+
+        // Libaray Model
+        private int id = 0;
+        private int libarayBookId = 0;
+        private int libarayStudentId = 0;
+        private DateTime subDate = DateTime.MinValue;
+        private DateTime retDate = DateTime.MinValue;
+        private int subscribed = 0;
+
+        // setting and getting values
+
+        // Student Properties
+
+        public string StudentName
+        {
+            get
+            {
+                return studentName;
+            }
+            set
+            {
+                studentName = value;
+            }
+        }
+        public int StudentRoll
+        {
+            get
+            {
+                return studentRoll;
+            }
+            set
+            {
+                studentRoll = value;
+            }
+        }
+        public int StudentMarks
+        {
+            get
+            {
+                return studentMarks;
+            }
+            set
+            {
+                studentMarks = value;
+            }
+        }
+        
+        // Book Properties
+
+        public int BookId
+        {
+            get { return bookId; }
+            set
+            {
+                bookId = value;
+            }
+        }
+        public string BookTittle
+        {
+            get { return tittle; }
+            set
+            {
+                tittle = value;
+            }
+        }
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                author = value;
+            }
+        }
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+            }
+        }
+
+        // Libaray Properties
+
+        public int LibarayId
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public int LibarayBookId
+        {
+            get
+            {
+                return libarayBookId;
+            }
+            set
+            {
+                libarayBookId = value;
+            }
+        }
+        public int LibarayStudentId
+        {
+            get
+            {
+                return libarayStudentId;
+            }
+            set
+            {
+                libarayStudentId = value;
+            }
+        }
+        public int Subscribed
+        {
+            get
+            {
+                return subscribed;
+            }
+            set
+            {
+                subscribed = value;
+            }
+        }
+        public DateTime SubDate
+        {
+            get
+            {
+                return subDate;
+            }
+            set
+            {
+                subDate = value;
+            }
+        }
+        public DateTime RetDate
+        {
+            get
+            {
+                return retDate;
+            }
+            set
+            {
+                retDate = value;
+            }
+        }
         public WelcomeForm()
         {
             InitializeComponent();
@@ -28,6 +188,21 @@ namespace College_Application
         }
 
         private void WelcomeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
